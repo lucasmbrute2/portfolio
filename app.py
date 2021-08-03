d60a069484f5216d5066e6f56c260aee9500c631
Lucas <lucasmbrute614@gmail.com>
2021-08-02 22:26:06 -0300

    Instaurando banco de dados

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, config, redirect
 from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
@@ -16,6 +18,10 @@ mail_settings = {
 app.config.update(mail_settings)
 
 mail = Mail(app)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cuoysxes:yM2YX50ph8obxJPs1YiEjzPR87cSTcsO@kesavan.db.elephantsql.com/cuoysxes'
+
 
 class Contato:
 
@@ -23,11 +29,57 @@ class Contato:
         self.nome = nome
         self.email = email
         self.mensagem = mensagem
+#--------------------------------------
+ #Cada class representa uma tabela sendo criada dentro do DB
+
+class Projeto(db.Model):
+    #Aqui é construido as colunas do DB
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    nome = db.Column(db.String(150), nullable=False)
+    imagem = db.Column(db.String(6000),nullable=False) #Aqui será usado apenas URLS de imagens, não serão feitos uploads.    
+    descricao = db.Column(db.String(600), nullable=False)
+    link = db.Column(db.String(1000), nullable=False)
+
+
+    def __init__(self, nome, imagem, descricao, link):
+            self.nome = nome
+            self.imagem =imagem                 
+            self.descricao = descricao                #Os atributos devem ter ter os mesmos nomes das colunas.
+            self.link = link
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/admin')
+def adm():
+    projetos = Projeto.query.all()
+    return render_template('adm.html', projetos= projetos) # no CRUD esse é o READ
+
+
+
+@app.route('/back_index')
+def back():
+    return redirect('/#projetos')
+
+
+#CRUD - CREATE
+@app.route('/new', methods=['GET','POST'])
+def new():
+    if request.method == 'POST':
+        projeto = Projeto(
+
+            request.form['nome'],
+            request.form['imagem'],
+            request.form['descricao'],
+            request.form['link']
+        )
+        db.session.add(projeto)
+        db.session.commit()
+        return redirect('/admin')
 
 
 @app.route('/send', methods =['GET', 'POST'])
@@ -47,9 +99,9 @@ def send():
             recipients = [app.config.get('MAIL_USERNAME'), 'lucasmbrute614@gmail.com'], 
             body = f'''
                 O {formContato.nome} com o e-mail {formContato.email}, envigou a seguinte mensagem:
-
                 {formContato.mensagem}
             '''
+
         
         )
         mail.send(msg)
@@ -60,6 +112,7 @@ def send():
 
 
 if __name__ == "__main__":  
+    db.create_all()
     app.run(debug=True)
 
 
